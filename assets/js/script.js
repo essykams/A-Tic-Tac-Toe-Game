0452eeba6f2e52c06bee7571d0b63bf23486dc67
essykams <>
2024-01-24 14:16:58 +0000

    assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 const board = document.getElementById('board');
+const resetButton = document.getElementById('reset-button');
 let currentPlayer = 'X';
 let moves = 0;
 let gameEnded = false;
+let playerXScore = 0;
+let playerOScore = 0;
 
+// Event listeners for cell click
 function handleCellClick(event) {
     if (gameEnded || event.target.textContent !== '') {
         return;
@@ -20,7 +24,7 @@ function handleCellClick(event) {
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     }
 }
-
+// Create the game board
 function createGameBoard() {
     for (let i = 0; i < 9; i++) {
         const cell = document.createElement('div');
@@ -29,7 +33,7 @@ function createGameBoard() {
         board.appendChild(cell);
     }
 }
-
+// Check for winner
 function checkWinner(player) {
     const cells = document.querySelectorAll('.cell');
     const winningCombinations = [
@@ -39,7 +43,7 @@ function checkWinner(player) {
         combination.every(index => cells[index].textContent === player)
     );
 }
-
+// Display the game result
 function displayResult(message) {
     const resultDisplay = document.createElement('div');
     resultDisplay.innerText = message;
@@ -47,4 +51,30 @@ function displayResult(message) {
     board.parentElement.appendChild(resultDisplay);
 }
 
+// Reset the game
+function resetGame() { 
+    const cells = document.querySelector('.cell');
+      cells.forEach(cell => {  
+        cell.textContent = '';
+      });
+      gameEnded =false;
+      moves = 0;
+}
+// Reset button event listener
+resetButton.addEventListener('click', resetGame);
 
+// Update player scores
+function updateScores(winner) {
+   if (winner === 'X') {  
+        playerXScore++;
+    } else if (winner=== 'O') { 
+        playerOScore++;
+    }
+
+    document.getElementById('player X').textContent = `player X:
+     $ {playerXScore}`;
+     document.getElementById('player O').textContent = `player O:
+     $ {playerOScore}`;
+}
+//  Initialize the game board
+createGameBoard();
